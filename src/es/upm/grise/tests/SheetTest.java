8929d793da80e7cc2e49e8574915b1d4d67c8e0f
Rcaccioppoli <Caccioppolirocco@gmail.com>
2016-12-10 10:12:24 +0100

    frist commit

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -1,14 +1,25 @@
 package es.upm.grise.tests;
 
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.*;
 
+import es.upm.grise.Sheet;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SheetTest {
 
+	private Sheet cells;
+
+	@Before
+	public void setUp() {
+		cells = new Sheet();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSet() {
+		cells.set("A1", "12");
+	    assertEquals("12", cells.get("A1"));
 	}
 
 }
